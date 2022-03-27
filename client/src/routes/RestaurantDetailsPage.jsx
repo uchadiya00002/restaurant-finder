d71a711a230199da28137c507f05f0352f7bafe4
Avinash Uchadiya <avinashuchadiya786@gmail.com>
2022-03-27 20:07:56 +0530

    add starrating component to display the ratings in star on details page as well as home page

--- client/src/routes/RestaurantDetailsPage.jsx
+++ client/src/routes/RestaurantDetailsPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import StarRating from "../components/StarRating";
 
 const RestaurantDetailsPage = () => {
   const { id } = useParams();
@@ -23,29 +24,7 @@ const RestaurantDetailsPage = () => {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      {selectedRestaurant && (
-        <>
-          <h1 className="text-center display-1">
-            {selectedRestaurant.restaurant.name}
-          </h1>
-          <div className="text-center">
-            {/* <StarRating rating={selectedRestaurant.restaurant.average_rating} /> */}
-            <span className="text-warning ml-1">
-              {selectedRestaurant.restaurant.count
-                ? `(${selectedRestaurant.restaurant.count})`
-                : "(0)"}
-            </span>
-          </div>
-          <div className="mt-3">
-            {/* <Reviews reviews={selectedRestaurant.reviews} /> */}
-          </div>
-          {/* <AddReview /> */}
-        </>
-      )}
-    </div>
-  );
+  return <div>{selectedRestaurant && <StarRating rating={3.3} />}</div>;
 };
 
 export default RestaurantDetailsPage;
--- server/db/db.sql
+++ server/db/db.sql
@@ -24,11 +24,12 @@
 -- to DROP or DELETE the database ==== DROP DATABASE  database_name;
 
 
-    CREATE TABLE restaurants (
+    CREATE TABLE reviews (
         id BIGSERIAL NOT NULL PRIMARY KEY,
+        restaurant_id  BIGINT NOT NULL REFERENCES restaurants(id),
         name VARCHAR(50) NOT NULL,
-        location VARCHAR(50) NOT NULL,
-        price_range INT NOT NULL check(price_range >=1 and  price_range<=5) );
+        review TEXT NOT NULL,
+        rating INT NOT NULL check(rating >=1 and  rating<=5) );
 
-        INSERT INTO restaurants (id, name , location , price_range) values (123, 'mcdonalds', 'new york',7 );
+        INSERT INTO reviews (name , review , rating) values ('Avinash', 'Restaurtant was awesome',4 );
     
